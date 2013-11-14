7c0d638da46ae2d8896e549ddf1fbd3193bf439c
Justin Hamilton <enigmarm@gmail.com>
2013-11-14 00:41:12 -0700

    changed order that map.js is being loaded. Doesn't get rid of console error. Needs more troubleshooting...but I need sleep first. Is anyone reading these messages? Oh ya, also tweaked color on hover for nav, in event of desktop users accessing.
    Signed-off-by: Justin Hamilton <enigmarm@gmail.com>

--- scripts/maps.js
+++ scripts/maps.js
@@ -4,13 +4,14 @@
 var map;
 
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(43.8120426, -111.798514),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('mapContainer'),
-      mapOptions);
+	var latLng= new google.maps.LatLng(43.8120426, -111.798514);
+	var mapOptions = {
+		zoom: 8,
+		center: latLng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('mapContainer'),
+		mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
